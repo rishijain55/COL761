c0b8f571fcdbafc86ac54748df717b2553765f94
rishijain55 <rishijain55@gmail.com>
2023-08-20 23:35:08 +0530

    final changes

---
+++ A1/compile.sh
@@ -0,0 +1,2 @@
+g++ --std=c++11 -o main src/main.cpp -O3;
+g++ --std=c++11 -o decompressor src/decompressor.cpp -O3; 
---
+++ A1/interface.sh
@@ -0,0 +1,7 @@
+if [ "$1" = "C" ]; then
+    ./main $2 $3
+fi
+
+if [ "$1" = "D" ]; then
+    ./decompressor $2 $3
+fi
